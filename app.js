bf6f8be16e0254c874cdcd5bf028776ef48bd12f
Nattpon Jp <119336552+Nattponmong@users.noreply.github.com>
2023-03-27 17:59:00 +0700

    add features

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const progressEl = document.querySelector(".progress");
 const buttonTop = document.querySelector(".top")
 const pageHeight = document.documentElement // --  refer to tag Html -- 
+const nav = document.querySelector("nav")
+const header = document.querySelector("header")
+
 window.onscroll = () => scrollProcress();
 
 //-- start ScrollProgress Bar --
@@ -15,17 +18,24 @@ function scrollProcress() {
   progressEl.style.width = scrollPercentage + "%";
 
   //-- End ScrollProgress Bar --
-
-
+      
   
   //--start ScrollTotop -- 
-  if (scrollPercentage > 30) { //-- 30%  --
+  if (scrollPercentage > 30 ) { //-- 30%  --
     buttonTop.classList.add("show-btn")
-
+    nav.classList.add("active")
+    nav.style.color="black"
+    header.classList.add("header-color")
   } else if (scrollPercentage === 0) {
     buttonTop.classList.remove("show-btn")
-
+    nav.classList.remove("active")
+    nav.style.color="white"
+    header.classList.remove("header-color")
+  }else{
+    
   }
+   
+
 
   // -- Event ScrollToTop ---
   buttonTop.addEventListener("click",scrollToTop)
